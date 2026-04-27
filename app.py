59fb26574b3424e00d72e2c9225daca3c501b756
Diksha2504 <diksha3335.beai24@chitkara.edu.in>
2026-04-27 17:08:29 +0530

    final changes for deployment

--- app.py
+++ app.py
@@ -677,5 +677,7 @@ def about():
     return render_template("about.html", logged_in=logged_in(),
                            username=session.get("username",""))
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
